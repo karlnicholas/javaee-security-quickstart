539a60779597675a8983aab98f7e9c15850ad2f8
Karl Nicholas <Karl.Nicholas@outlook.com>
2015-12-02 19:10:40 -0500

    Full testing with Arquillian/Drone/Graphene

--- src/test/java/test/ControllerTest.java
+++ src/test/java/test/ControllerTest.java
@@ -1,6 +1,8 @@
 package test;
 
 import static org.jboss.arquillian.graphene.Graphene.guardHttp;
+import static org.jboss.arquillian.graphene.Graphene.guardAjax;
+
 import static org.junit.Assert.*;
 
 import java.io.File;
@@ -85,46 +87,133 @@ public class ControllerTest {
     @Drone
     private WebDriver browser;
 
-    @ArquillianResource
-    private URL deploymentUrl;
-
-    @FindBy(id = "registerForm:email")
-    private WebElement email;
-
-    @FindBy(id = "registerForm:password")
-    private WebElement password;
-
-    @FindBy(id = "registerForm:firstName")
-    private WebElement firstName;
-
-    @FindBy(id = "registerForm:lastName")
-    private WebElement lastName;
-
-    @FindBy(id = "registerForm:register")
-    private WebElement registerButton;
-    
-    @FindBy(name = "messages")        // find last occurrence of name="messages" 
-    private WebElement facesMessage;
-    
     @Test
     public void testRegister() throws Exception {
-       browser.get(deploymentUrl.toExternalForm() + "views/register.xhtml");
-       
-       email.sendKeys("karl@karl.com");
-       password.sendKeys("1234");
-       firstName.sendKeys("Karl");
-       lastName.sendKeys("Nicholas");
+        // only one user should be registered
+        browser.get(deploymentUrl.toExternalForm() + "");
+        guardAjax(showUserCountButton).click();
+        assertEquals("There are 1 users", numberOfUsers.getText().trim());
+        
+        // login should fail
+        browser.get(deploymentUrl.toExternalForm() + "views/account.xhtml");        
+        loginsignupEmail.sendKeys("karl@karl.com");
+        loginsignupPassword.sendKeys("1234");
+        guardHttp(loginButton).click();
+        assertEquals("Login Failed!", loginsignupMessages.getText().trim());
 
-       guardHttp(registerButton).click();
-       
-       assertEquals("Registration Successful!", facesMessage.getText().trim());
+        // register a new user
+        browser.get(deploymentUrl.toExternalForm() + "views/register.xhtml");
+        registerEmail.sendKeys("karl@karl.com");
+        registerPassword.sendKeys("1234");
+        registerFirstName.sendKeys("Karl");
+        registerLastName.sendKeys("Nicholas");
+        guardHttp(registerButton).click();
+        assertEquals("Registration Successful!", detailMessages.getText().trim());
 
-/*        
-        whoAmI.click();
-        waitAjax().until().element(signedAs).is().present();
-        assertTrue(signedAs.getText().contains("demo"));
-*/        
+        // update details, from /views/account.xhtml
+        userdetailFirstName.clear();
+        userdetailFirstName.sendKeys("Karl");
+        userdetailLastName.clear();
+        userdetailLastName.sendKeys("Nicholas");
+        guardHttp(updateButton).click();
+        assertEquals("User Info Updated", detailMessages.getText().trim());
+
+        // now two users should be registered
+        browser.get(deploymentUrl.toExternalForm() + "views/index.xhtml");
+        assertEquals("Welcome", welcomeMessage.getText().trim());
+        assertEquals("Karl.", welcomeUserMessage.getText().trim());
+        guardAjax(showUserCountButton).click();
+        assertEquals("There are 2 users", numberOfUsers.getText().trim());
+
+        // logout
+        browser.get(deploymentUrl.toExternalForm() + "views/account.xhtml");
+        guardHttp(logoutButton).click();
+        
+        // change password
+        browser.get(deploymentUrl.toExternalForm() + "views/user/changepassword.xhtml");
+        // first login with j_security login form
+        loginJ_username.sendKeys("karl@karl.com");
+        loginJ_password.sendKeys("1234");
+        guardHttp(loginSubmitButton).click();
+        // then change password
+        changepasswordPassword.sendKeys("1234");
+        changepasswordConfirmPassword.sendKeys("1234");
+        guardHttp(updatePasswordButton).click();
+        assertEquals("Password Updated", detailMessages.getText().trim());
+        
+        // logout from account page
+        guardHttp(logoutButton).click();
+
+        // login should fail
+        browser.get(deploymentUrl.toExternalForm() + "views/account.xhtml");        
+        loginsignupEmail.sendKeys("admin");
+        loginsignupPassword.sendKeys("admin");
+        guardHttp(loginButton).click();
+        assertTrue(adminButton.isDisplayed());
+
+        // perform admin functions on user
+        browser.get(deploymentUrl.toExternalForm() + "views/admin/admin.xhtml");
+        guardHttp(promoteButton).click();
+        assertEquals("User promoted to administrator", adminMessages.getText().trim());
+        guardHttp(demoteButton).click();
+        assertEquals("User demoted to user only", adminMessages.getText().trim());
+        guardHttp(removeButton).click();
+        assertEquals("User removed", adminMessages.getText().trim());
+
+        // logout
+        browser.get(deploymentUrl.toExternalForm() + "views/account.xhtml");
+        guardHttp(logoutButton).click();
+
+        // only one user should be registered
+        browser.get(deploymentUrl.toExternalForm() + "");
+        guardAjax(showUserCountButton).click();
+        assertEquals("There are 1 users", numberOfUsers.getText().trim());
+        
+        // done
     }
 
+    @ArquillianResource private URL deploymentUrl;
 
+    // index page
+    @FindBy(id = "indexForm:welcomeMessage") private WebElement welcomeMessage;
+    @FindBy(id = "indexForm:welcomeUserMessage") private WebElement welcomeUserMessage;
+    @FindBy(id = "indexForm:showUserCount") private WebElement showUserCountButton;
+    @FindBy(id = "indexForm:numberOfUsers") private WebElement numberOfUsers;
+    
+    // account page/loginsignup fragment
+    @FindBy(id = "loginsignupForm:login") private WebElement loginButton;
+    @FindBy(id = "loginsignupForm:email") private WebElement loginsignupEmail;
+    @FindBy(id = "loginsignupForm:password") private WebElement loginsignupPassword;
+    @FindBy(name = "loginsignupForm:messages") private WebElement loginsignupMessages;
+
+    // Register Page
+    @FindBy(id = "registerForm:email") private WebElement registerEmail;
+    @FindBy(id = "registerForm:password") private WebElement registerPassword;
+    @FindBy(id = "registerForm:firstName") private WebElement registerFirstName;
+    @FindBy(id = "registerForm:lastName") private WebElement registerLastName;
+    @FindBy(id = "registerForm:register") private WebElement registerButton;
+    
+    // userdetail Page
+    @FindBy(id = "userdetailForm:firstName") private WebElement userdetailFirstName;
+    @FindBy(id = "userdetailForm:lastName") private WebElement userdetailLastName;
+    @FindBy(id = "userdetailForm:logout") private WebElement logoutButton;
+    @FindBy(id = "userdetailForm:update") private WebElement updateButton;
+    @FindBy(id = "userdetailForm:admin") private WebElement adminButton;
+    @FindBy(name = "userdetailForm:messages") private WebElement detailMessages;
+
+    // j_security login Page
+    @FindBy(id = "j_username") private WebElement loginJ_username;
+    @FindBy(id = "j_password") private WebElement loginJ_password;
+    @FindBy(id = "submit") private WebElement loginSubmitButton;
+
+    // changepassword Page
+    @FindBy(id = "changepasswordForm:password") private WebElement changepasswordPassword;
+    @FindBy(id = "changepasswordForm:confirmPassword") private WebElement changepasswordConfirmPassword;
+    @FindBy(id = "changepasswordForm:updatePassword") private WebElement updatePasswordButton;
+
+    // admin Page
+    @FindBy(id = "adminForm:users:1:promote") private WebElement promoteButton;
+    @FindBy(id = "adminForm:users:1:demote") private WebElement demoteButton;
+    @FindBy(id = "adminForm:users:1:remove") private WebElement removeButton;
+    @FindBy(name = "adminForm:messages") private WebElement adminMessages;
 }
