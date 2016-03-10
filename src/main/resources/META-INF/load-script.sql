1941add734a8fd6a9821261b7f6a0b55b3edd556
Karl Nicholas <Karl.Nicholas@outlook.com>
2016-03-10 17:52:53 -0500

    updates to readme.

--- src/main/resources/META-INF/load-script.sql
+++ src/main/resources/META-INF/load-script.sql
@@ -1,5 +1,5 @@
 INSERT INTO role(ID, role) VALUES (1, 'USER');
 INSERT INTO role(ID, role) VALUES (2, 'ADMIN');
-insert into user( id ,email ,firstName ,lastName ,password ) values ( 1, 'admin@test.com', 'Admin', 'Istrator', 'bXW7XpdiR775awTTzf4q6XkZ4uShMkfwffLMGPzoTTc=');
+insert into user( id ,email ,firstName ,lastName ,password ) values ( 1, 'admin@test.com', 'Admin', 'Istrator', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=');
 insert into user_role(user_id ,roles_id ) values (1, 1);
 insert into user_role(user_id ,roles_id ) values (1, 2);
